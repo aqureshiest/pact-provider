97dfb59aea317924106627ebd1aa9b6df501b648
aqureshiest <adeel.qureshi@earnest.com>
2020-04-12 14:58:55 -0700

    u

--- src/test/java/com/example/provider/PeopleControllerTest.java
+++ src/test/java/com/example/provider/PeopleControllerTest.java
@@ -8,24 +8,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PeopleControllerTest {
 
     @Autowired
-    TestRestTemplate restTemplate;
+    TestRestTemplate server;
 
     @Test
-    public void all_people() {
-        String response = restTemplate.getForObject("http://localhost:8081/people", String.class);
+    public void get_all_people() {
+        String response = server.getForObject("http://localhost:8081/people", String.class);
         Assertions.assertThat(response).isNotEmpty();
+        // verify other properties
     }
 
     @Test
     public void person_by_id() {
-        String response = restTemplate.getForObject("http://localhost:8081/people/1", String.class);
+        String response = server.getForObject("http://localhost:8081/people/1", String.class);
         Assertions.assertThat(response).isNotEmpty();
     }
 }

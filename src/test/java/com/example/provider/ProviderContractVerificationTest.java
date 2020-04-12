fe4f984698e945334982ec6bad9afb4415b1a6a3
aqureshiest <adeel.qureshi@earnest.com>
2020-04-12 01:16:39 -0700

    added pact verification

---
+++ src/test/java/com/example/provider/ProviderContractVerificationTest.java
@@ -0,0 +1,27 @@
+package com.example.provider;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.SpringRestPactRunner;
+import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Provider("provider")
+@PactFolder("../consumer/target/pacts")
+@RunWith(SpringRestPactRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProviderContractVerificationTest {
+
+	@TestTarget
+	public final Target target = new SpringBootHttpTarget();
+
+
+	@State("has people")
+	public void contextLoads() {
+	}
+
+}
